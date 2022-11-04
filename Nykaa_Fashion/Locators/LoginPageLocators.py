355522fc093f37f11e576309e4365d8c62585aa6
abrahmbhatt1610 <117143737+abrahmbhatt1610@users.noreply.github.com>
2022-11-04 00:46:01 -0500

    Add files via upload

---
+++ Nykaa_Fashion/Locators/LoginPageLocators.py
@@ -0,0 +1,6 @@
+account_button = '[class="css-1tef76f"]'
+login_button = 'button[data-at="login-btn"]'
+cont_number = '[aria-label ="Mobile number"]'
+submit_button = 'button[type="submit"]'
+verify_otp = 'button[type="submit"]'
+wrong_otp_number = '[class="css-132pekl"] span'
